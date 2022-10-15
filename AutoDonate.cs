4297c5833a9542fb49a9def9dbf535988bbde587
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-10-15 14:28:19 +0300

    Update AutoDonate.cs

--- AutoDonate.cs
+++ AutoDonate.cs
@@ -1,1 +1,21 @@
+// Версия AutoDonate: 0.1
+// Название: AutoDonate
+// Описание:
+
+//
+//    ___       __       ___                __     
+//   / _ |__ __/ /____  / _ \___  ___ ___ _/ /____ 
+//  / __ / // / __/ _ \/ // / _ \/ _ / _ `/ __/ -_)
+// /_/ |_\_,_/\__/\___/____/\___/_//_\_,_/\__/\__/ 
+//                                                 
+
+using System
+using System.Collections.Generic;
+using System.Reflection;
+using Newtonsoft.Json;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI
+using TShockAPI.DB
+
 
